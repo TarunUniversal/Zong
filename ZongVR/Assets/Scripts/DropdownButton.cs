7c8b908eb15fdb7078cc3d8ce8fc89973fe4b458
TARUN RATHORE <tarunrathore170899@gmail.com>
2022-12-18 22:32:49 +0530

    Environment setup & build UI.

---
+++ ZongVR/Assets/Scripts/DropdownButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropdownButton : MonoBehaviour
+{
+    [SerializeField] private GameObject _dropdownItems;
+    private bool _isOpen = false;
+    
+    public void OnDropdownButtonClick()
+    {
+        if(!_isOpen)
+        {
+            _dropdownItems.SetActive(true);
+            _isOpen = !_isOpen;
+        }
+        else
+        {
+            _dropdownItems.SetActive(false);
+            _isOpen = !_isOpen;
+        }
+    }
+}
